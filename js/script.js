80c095d22b97d348c3370deb83688811cf90518b
Gustavo Voltarel Deodato da Silva <gustavo.voltarel@santacasasc.local>
2021-09-29 09:42:54 -0300

    :package: Footer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 document.querySelector(".menu-inicial").addEventListener("click", () => document.querySelector(".container").classList.toggle("show-menu"))
+
+document.querySelector("#qtde").addEventListener("changed", attPreco)
+
+attPreco => {
+const qtde = document.querySelector("#qtde").nodeValue
+}
